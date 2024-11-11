f19694a3b1ef131ba1bda6a69f1ac9b2a88b4e8a
Anne Kehlet <anne.kehlet@equalexperts.com>
2024-11-11 16:41:37 +0000

    Proto v1 links added
    Adde correct links to
    Manage your claims
    Agreement summary
    Email about change

--- app/views/prototypes/v01/routes.js
+++ app/views/prototypes/v01/routes.js
@@ -7,15 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit');
 const router = govukPrototypeKit.requests.setupRouter();
 
 // Generic route for capturing form data and redirecting
-router.post('/prototypes/v01/views/:nextPage', (req, res) => {
-  req.session.data = {
-    ...req.session.data,
-    ...req.body
-  };
-  console.log('Session data:', req.session.data); 
-  const nextPage = req.params.nextPage;
-  res.redirect(`/prototypes/v01/views/${nextPage}`);
-});
+ router.post('/prototypes/v01/views/:nextPage', (req, res) => {
+   req.session.data = {
+     ...req.session.data,
+     ...req.body
+   };
+   console.log('Session data:', req.session.data); 
+   const nextPage = req.params.nextPage;
+   res.redirect(`/prototypes/v01/views/${nextPage}`);
+ });
+
+
 
 
 // Route from holding page
@@ -24,11 +26,13 @@ router.get('/next-task-v1', function (req, res) {
 })
 
 
-
 // Dynamic route for rendering pages within this prototype
 router.get('/*', (req, res) => {
   const page = req.params[0];
   res.render('prototypes/v01/views/' + page);
 });
+
+
+
 
 module.exports = router;
