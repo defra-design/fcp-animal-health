e14de6ecaec700b994b98537f62ac776c000ba5d
Anne Kehlet <anne.kehlet@equalexperts.com>
2025-03-24 10:20:15 +0000

    Research data for scenarios and variables on Check your answers
    Updated Static backup data for scenarios
    
    Updated variables for check your details page. Fixed variables for dates

--- app/filters.js
+++ app/filters.js
@@ -7,3 +7,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
+
+addFilter('toMonth', function (x) { 
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];   
+    if (x > 0){ return months[x - 1]; // returns date as per month      
+    } else {
+            return x ;      
+    }}) 
+
+addFilter('toMoney', function(x){  return("£" + x );
+    //TO ADD - case to handle nothing being there 
+    })
