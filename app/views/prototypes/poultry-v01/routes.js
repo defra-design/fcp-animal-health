e861490960f45bd60c3d83b3fa096dff8c00df50
Carlos Bernardo Jr <carlos@cbernardojr.com>
2026-01-29 15:40:23 +0000

    Poultry joruney

---
+++ app/views/prototypes/poultry-v01/routes.js
@@ -0,0 +1,31 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit');
+const router = govukPrototypeKit.requests.setupRouter();
+
+// Generic route for capturing form data and redirecting
+ router.post('/prototypes/poultry-v01/views/:nextPage', (req, res) => {
+   req.session.data = {
+     ...req.session.data,
+     ...req.body
+   };
+   console.log('Session data:', req.session.data); 
+   const nextPage = req.params.nextPage;
+   res.redirect(`/prototypes/poultry-v01/views/${nextPage}`);
+ });
+
+// Route from holding page
+router.get('next-task-v5', function (req, res) {
+  res.render('prototypes/poultry-v01/views/index-research.njk') // 'index' for the v05 folder
+})
+
+// Dynamic route for rendering pages within this prototype
+router.get('/*', (req, res) => {
+  const page = req.params[0];
+  res.render('prototypes/poultry-v01/views/' + page);
+});
+
+module.exports = router;
